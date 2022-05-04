19300095cbd0208312a700df410f33c4b06de99c
jacobdoesaudio <jacob@reddeskaudio.com>
2022-05-04 14:56:11 -0700

    Version 0.5

--- Bin/3D Audio Exhibit_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Bin/3D Audio Exhibit_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,7 +14,13 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_GROUP___0014___DRUMS = 3433433191U;
+        static const AkUniqueID PLAY_GROUP___0016___BASS = 1246022167U;
         static const AkUniqueID PLAY_GROUP___0020___KEYS = 3596766455U;
+        static const AkUniqueID PLAY_GROUP___0028___SYNTHS = 294034842U;
+        static const AkUniqueID PLAY_GROUP___0037___GTR = 2912176820U;
+        static const AkUniqueID PLAY_GROUP___0039___SAX = 2044558763U;
+        static const AkUniqueID PLAY_GROUP___0046___VOCALS = 4166858865U;
+        static const AkUniqueID PLAY_GROUP___0060___FX_REDUX = 1211835638U;
     } // namespace EVENTS
 
     namespace BANKS
